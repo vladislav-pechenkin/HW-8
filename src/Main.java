4888a7b2880f8d48ff2b02a57426d7d238d76fb1
User <89028637729@mail.ru>
2024-07-11 17:43:05 +0500

    Домашняя работа № 8 Сделана

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -21,8 +23,79 @@ public class Main {
             System.out.println(weights2[i]);
         }
 
+        System.out.println();
         System.out.println("Задача № 2");
 
+        int[] weights3 = new int[12];
+        weights3[0] = 1;
+        weights3[1] = 2;
+        weights3[2] = 3;
+        for (int i = 0; i < weights3.length; i++) {
+            System.out.print(weights3[i]);
+            if (i < weights3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
+        double[] weights4 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < weights4.length; i++) {
+            System.out.print(weights4[i]);
+            if (i < weights4.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        double[] weights5 = {90, 91, 93, 92, 85, 87, 84, 83, 1.57, 7.654, 9.986};
+        for (int i = 0; i < weights5.length; i++) {
+            System.out.print(weights5[i]);
+            if (i < weights5.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Задача № 3");
+
+        int[] weights6 = new int[12];
+        weights6[0] = 1;
+        weights6[1] = 2;
+        weights6[2] = 3;
+        for (int i = weights6.length - 1 ; i >= 0; i--) {
+            System.out.print(weights6[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double[] weights7 = {1.57, 7.654, 9.986};
+        for (int i = weights7.length - 1 ; i >= 0; i--) {
+            System.out.print(weights7[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double[] weights8 = {90, 91, 93, 92, 85, 87, 84, 83, 1.57, 7.654, 9.986};
+        for (int i = weights8.length - 1 ; i >= 0; i--) {
+            System.out.print(weights8[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println("Задача № 4");
+
+        int[] weights9 = new int[12];
+        weights9[0] = 1;
+        weights9[1] = 2;
+        weights9[2] = 3;
+        for (int i = 0; i < weights9.length; i++) {
+            if (weights9[i] % 2 != 0 ){
+                weights9[i] += 1;
+            }
+
+        }
+        System.out.println(Arrays.toString(weights9));
     }
 }
